9bf37581ad714581a9ba11592e35b9f6821ed941
Avinash Mehrotra <a281983@gmail.com>
2020-03-20 14:40:52 +0000

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,21 @@
+
+export default {
+
+s3: {
+REGION: "us-east-1",
+BUCKET: "notesapp-upload-docs"
+},
+
+apiGateway: {
+REGION: "us-east-2",
+URL: "https://0ix9n1ieij.execute-api.us-east-2.amazonaws.com/prod" 
+},
+
+cognito: {
+REGION: "us-east-2",
+USER_POOL_ID: "us-east-2_YBckur0Bq",
+APP_CLIENT_ID: "661ooqajravrhvgskcsr4ag8ut",
+IDENTITY_POOL_ID: "us-east-2:f4db533f-b84c-43b3-a017-e6852ad9ab2a"
+}
+
+};
